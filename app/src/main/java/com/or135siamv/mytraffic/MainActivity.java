8a167e7bd1572a0aae3b77cbce7795cf25b755de
or135 <or135@hotmail.com>
2016-06-05 10:03:38 +0700

    Start 5 June 16

--- app/src/main/java/com/or135siamv/mytraffic/MainActivity.java
+++ app/src/main/java/com/or135siamv/mytraffic/MainActivity.java
@@ -1,6 +1,8 @@
 package com.or135siamv.mytraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     //Explicit หรือ การประกาศตัวแปร
     private ListView trafficListView;
     private Button aboutMeButton;
+    private String urlYoutubestring = "https://youtu.be/GVVW4Mx9SFE";
+
+
 
 
 
@@ -27,8 +32,6 @@ public class MainActivity extends AppCompatActivity {
         //Controller buttom
         controllerButton();
 
-
-
     } // Main Method หรือ เมธอดหลัก
 
     private void controllerButton() {
@@ -37,16 +40,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //Sound Effect
+                // การใส่เสียงทำซาวด์เอฟเฟคประกอบการคลิกปุ่ม
                 MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cat);
                 mediaPlayer.start();
 
 
+                //web View
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(urlYoutubestring));
+                startActivity(intent);
+                
+
             }
         });
 
-        // controllerButton
-    }
+
+    }// controllerButton
 
 
 }  // Main Class นี่คือคลาสหลัก
+
+
